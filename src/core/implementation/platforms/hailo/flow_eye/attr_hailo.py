4831cc33f49b35a8d6a6109a8ba6bbb73e7f5d4a
nixon <nixondutta402@gmail.com>
2025-03-19 12:50:42 +0900

    delete uncessary files

--- src/core/implementation/platforms/hailo/flow_eye/attr_hailo.py
+++
@@ -1,6 +0,0 @@
-from gsthailo import VideoFrame
-from gi.repository import Gst
-
-
-def run(video_frame: VideoFrame):
-    return Gst.FlowReturn.OK
--- src/core/implementation/platforms/hailo/flow_eye/pipelines/test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-print(
-    os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "hailopython", "class_hailo.py"
-    )
-)

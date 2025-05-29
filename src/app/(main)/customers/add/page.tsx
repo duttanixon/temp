43939960a6ed208c3eb682ff4905dce66f534c78
yoshida-cyber <rion.yoshida@cybercore.co.jp>
2025-05-29 14:58:01 +0900

    add addPage-Tabs

--- src/app/(main)/customers/add/page.tsx
+++ src/app/(main)/customers/add/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useState } from "react";
-import { TabsNav } from "../_components/TabsNav";
 import CustomerCreateForm from "./_components/CustomerCreateForm";
 import {
   Breadcrumb,
@@ -12,7 +10,6 @@ import {
 } from "@/components/ui/breadcrumb";
 
 export default function AddCustomerPage() {
-  const [activeTab, setActiveTab] = useState("basic");
   return (
     <div>
       <Breadcrumb className="text-sm text-[#7F8C8D]">
@@ -27,10 +24,7 @@ export default function AddCustomerPage() {
       <div className="flex flex-col gap-8 text-2xl font-bold text-[#2C3E50]">
         顧客の作成
         <section className="flex flex-col gap-4">
-          <div className="inline-flex w-fit border border-[#BDC3C7] rounded bg-[#FFFFFF] overflow-hidden">
-            <TabsNav activeTab={activeTab} onChange={setActiveTab} />
-          </div>
-          <CustomerCreateForm activeTab={activeTab} />
+          <CustomerCreateForm />
         </section>
       </div>
     </div>

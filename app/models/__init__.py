dc9cc7971c05d4d15bd0415dc5df3463679dc9f4
nixon <dutta@cybercore.co.jp>
2025-05-30 11:29:59 +0900

    change to pascal case for table name CityEyeHumanTable, CityEyeTrafficTable

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,5 @@ from app.models.device import Device, DeviceStatus, DeviceType
 from app.models.solution import Solution, SolutionStatus
 from app.models.customer_solution import CustomerSolution, LicenseStatus
 from app.models.device_solution import DeviceSolution, DeviceSolutionStatus
-from app.models.services.city_eye.human_table import City_Eye_human_table
-from app.models.services.city_eye.traffic_table import City_Eye_traffic_table
+from app.models.services.city_eye.human_table import CityEyeHumanTable
+from app.models.services.city_eye.traffic_table import CityEyeTrafficTable
--- app/models/services/city_eye/human_table.py
+++ app/models/services/city_eye/human_table.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, String, DateTime, ForeignKey, Integer
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
-class City_Eye_human_table(Base):
+class CityEyeHumanTable(Base):
     __tablename__ = "city_eye_human_data"
 
     data_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
